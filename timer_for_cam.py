19bc11fe018c7f2c3bfc89b37fb84f15cee34d91
Sid <sidney.frey@student.uni-tuebingen.de>
2025-08-12 08:35:00 +0200

    modified:   timer_for_cam.py

--- timer_for_cam.py
+++ timer_for_cam.py
@@ -5,6 +5,7 @@ from datetime import datetime
 # Defintionen
 start_times = ["00:00", "12:00"]
 
+# Checken, ob aktuell eine Startzeit ist, wenn ja: Kamera starten und entsprechend abspeichern
 while True:
     # Zeit holen
     time_now = datetime.now().strftime("%H:%M")
@@ -19,6 +20,7 @@ while True:
         else:
             file_name = f"error_{date_now}_{time_now}.mp4"
 
+        # Kamerastart über Terminal
         command = f"./minimal_pylon -f 10 -q 15 -n 70000 -c 2 -w 2200 -h 1600 -x 500 -y 500 -e 30000 -g1 10.0 -g2 8.8 -o {file_name}"
 
         try:
